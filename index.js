5a27f3c3ad5371c5dd771c628975d9576215093b
Leslie Stevens <flymaven@protonmail.com>
2019-10-17 20:02:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+function changeText() {
+  document.querySelector("#text").textContent = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  changeText()
+})
