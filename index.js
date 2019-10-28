28af5dc0ac4cfb86eb12d557931c6e33ae743555
Sushi Chanrai <sushi@pathworks.co>
2019-10-28 15:30:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let element = document.querySelector("p#text");
+  element.innerHTML = 'This is really cool!';
+});
