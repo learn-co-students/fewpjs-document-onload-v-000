2d50c2bf31cf8d1a8fb62594e1f471db77afc073
Heber Sandoval <he3ers@gmail.com>
2020-01-18 08:36:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Your code goes here
 document.addEventListener('DOMContentLoaded', function() {
   updateTheDOM();
+  // document.getElementById( "text" ).innerHTML = "This is really cool!";
 });
 
 function updateTheDOM() {
-  document.getElementById( "text" ).innerHTML = "This is really cool!";
+  document.getElementById("text").innerHTML = "This is really cool!";
 }
