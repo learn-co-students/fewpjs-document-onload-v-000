6c5dd4cea9b16b11fa0cf5de038b9dff4b4578f2
carolgreene <carolgreene3@gmail.com>
2020-02-25 18:13:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+//document.addEventListener("DOMContentLoaded", 
+//function() {
+//  let phrase = document.getElementById("text")
+//  phrase.innerHTML = "This is really cool!"
+//})
+
+
+//solution had event listener call the function instead of doing everything w/in the listener
+
+document.addEventListener("DOMContentLoaded",
+function(){
+  updateDOM()
+})
+
+//did not need to use a variable, but can if I want to
+
+function updateDOM() {
+  document.getElementById("text")
+  .innerHTML = "This is really cool!"
+}
