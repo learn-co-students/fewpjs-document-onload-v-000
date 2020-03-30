2736bac89e16c85e6599b41b17bc081b8f29a0d6
Kat Tochowicz <ktochow1@binghamton.edu>
2020-03-30 01:15:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+//DOMContentLoaded = browser's built-in way to show when page is loaded
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("text").innerHTML = "This is really cool!";
+
+});
