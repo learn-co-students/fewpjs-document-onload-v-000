0b8d8535a6965cadce11f7de2b00b2f21d55f983
dani8439 <dani8439@aol.com>
2020-05-22 13:31:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 // Your code goes here
 
-// document.addEventListener("DOMContentLoaded",
-// function() {
-//   console.log("The DOM has loaded");
-// });
-//
-// console.log(
-//   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-// );
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
 
 
-document.addEventListener("DOMContentLoaded",
-function() {
+document.addEventListener("DOMContentLoaded", function () {
   updateDOM();
 });
 
-function updateDOM() {
-  document.getElementById('text').textContent = "This is really cool!";
-  // document.getElementById('text').innerHTML = "This is really cool!"; .innerHTML and .textContent are interchangeable here. 
+function updateDOM(){
+  document.getElementById('text').textContent = "This is really cool!"
+  // document.getElementById('text').innerHTML -- innerHTML and textContent are interchangeable.
 }
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   updateDOM();
-// });
-//
-// function updateDOM() {
-//   document.getElementById('text').textContent = 'This is really cool!';
-// }
