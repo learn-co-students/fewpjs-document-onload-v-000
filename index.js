a203444169e468f076c9448deffc2a0ba4b90922
Eric Johnson <ericjohnson0216@icloud.com>
2020-08-21 22:49:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// let element = document.getElementById("text");
+// element.innerHTML = 'This is really cool!';
+
+document.addEventListener("DOMContentLoaded", function () {
+    let element = document.getElementById("text");
+    element.innerHTML = 'This is really cool!';
+    // console.log("JavaScript is so cool. It lets me add text to my page programmatically.");
+});
+
+// let element = document.getElementById("text");
+// element.innerHTML = 'This is really cool!'
