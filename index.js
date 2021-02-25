b54c00702c9f3ce46e4a2c2553fc9352c1740a56
capricious-slingshot <ephemere@protonmail.com>
2021-02-25 13:11:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const text = "This is really cool!"
+
+const wrapper = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("The DOM has loaded")
+  wrapper.textContent = text
+})
+
