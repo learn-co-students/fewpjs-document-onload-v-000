6e4bcdc9ff1dbad723b67db03567f03dfa32cd03
guysbryant <guy.s.bryant@gmail.com>
2021-04-25 00:00:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    changeP(); 
+})
+
+function changeP(){
+    document.querySelector("p#text").textContent = "This is really cool!";
+}
